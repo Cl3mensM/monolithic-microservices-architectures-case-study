225bd92c14f0634aba7553020b266d4fab323251
Clemens Marx <clemens.marx@gmail.com>
2025-03-17 22:12:51 +0100

    Finish stations arrival times service with dummy data

--- monolithic_architecture/stations/models.py
+++ monolithic_architecture/stations/models.py
@@ -1,3 +1,3 @@
 from django.db import models
 
-# Create your models here.
+# Add model for station arrival times, if needed
--- monolithic_architecture/stations/views.py
+++ monolithic_architecture/stations/views.py
@@ -5,4 +5,22 @@ from django.http import JsonResponse
 
 @api_view(['POST'])
 def stations(request):
-    return JsonResponse({"status": "Okay"}, status=200)
+    try:
+        station_id = request.data['station_id']
+        station_name = request.data['station_name']
+
+        # Handle arrival times for exact station, if needed
+        # For now, just return some dummy data
+
+        response = {
+            "station_id": station_id,
+            "station_name": station_name,
+            "next_arrivals": [
+                2, 4, 6, 8, 10
+            ]
+        }
+
+        return JsonResponse(response, status=200)
+
+    except KeyError:
+        return JsonResponse({"status": "Bad Request, invalid request body."}, status=400)
